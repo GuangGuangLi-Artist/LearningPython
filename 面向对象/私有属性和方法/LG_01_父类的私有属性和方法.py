f9abe849caa8545c06bb5234ca7d76ab625a8306
GuangGuangLi <15607521232@163.com>
2019-09-26 02:54:40 +0800

    update

---
+++ 面向对象/私有属性和方法/LG_01_父类的私有属性和方法.py
@@ -0,0 +1,31 @@
+#coding=utf-8
+
+class A:
+    def __init__(self):
+        self.num1 = 100
+        self.__num2 = 200
+
+
+    def __test(self):
+        print("私有方法 %d %d" %(self.num1,self.__num2))
+
+
+
+class B(A):
+    def demo(self):
+
+        #在子类的对象方法中，不能访问父类的私有属性，也不能调用父类的私有方法
+        #print("访问父类的私有属性 %d", %self.__num2)
+
+        #self.__test()
+        pass
+
+
+#创建一个子类对象
+b = B
+print(b)
+#在外界不能直接访问对象的私有属性和私有方法
+
+print(b.num1)
+#print(b.__num2)
+#b.__test
