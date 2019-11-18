6a98db1cbfff40b52da8bd67ed8b6762b6c748b6
GuangGuangLi <15607521232@163.com>
2019-11-18 18:04:26 +0800

    update

--- 就业班/网络编程/socket下载服务器/LG_01_下载器客户端.py
+++ 就业班/网络编程/socket下载服务器/LG_01_下载器客户端.py
@@ -8,21 +8,21 @@ def main():
     tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     #目的地信息
-    server_ip = input("请输入服务器ip:")
-    server_port = input("请输入目的地端口:")
+    #server_ip = input("请输入服务器ip:")
+    #server_port = int(input("请输入目的地端口:"))
 
     #链接服务器
-    tcp_client_socket.connect(server_ip,server_port)
-
+    #tcp_client_socket.connect((server_ip,server_port))
+    tcp_client_socket.connect(('45.76.213.222', 7890))
     #输入需要下载的文件名
     file_name = input("请输入要下载的文件名:")
 
     #发送文件下载请求
     tcp_client_socket.send(file_name.encode("utf-8"))
 
-    #接收服务器发送回来的数据，一次最大接受1k(1024)
+    #接收服务器发送回来的数据，一次最大接受1k(1024*1024)
 
-    recev_data = tcp_client_socket.recv(1024)
+    recev_data = tcp_client_socket.recv(1024*1024)
 
     print("接收到的数据为:",recev_data.decode("utf-8"))
 
--- 就业班/网络编程/socket下载服务器/LG_02_下载器服务端.py
+++ 就业班/网络编程/socket下载服务器/LG_02_下载器服务端.py
@@ -2,33 +2,36 @@
 
 
 import socket
-import sys
 
 
-def get_file_content(file_name):
-    """获取文件内容"""
+def send_file_2_client(new_client_socket,client_addr):
+    #接收客户端需要下载的文件名
+    file_name = new_client_socket.recv(1024).decode("utf-8")
+    print("客户端(%s)需要下载的文件是: %s" % (str(client_addr),file_name))
+
+    file_content = None
+
+    #打开文件，读取数据
     try:
-        with open(file_name,"rb") as f:
-            content =f.read()
-        return content
-    except:
-        print("没有下载的文件: %s" %file_name)
+        f = open(file_name,"rb")
+        file_content = f.read()
+        f.close()
+    except Exception as res:
+        print("下载的文件(%s)不存在" % file_name)
 
+    #发送文件的数据给客户端
+    if file_content:
+        new_client_socket.send(file_content)
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("请按照如下方式运行: python3 xxx.py 7890")
-        return
-    else:
-        #运行方式为python3 xxx.py 7890
-        port = int(sys.argv[1])
+
 
     #创建socket
     tcp_server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
     #本地信息
-    address = ('',port)
+    address = ('45.76.213.222',7890)
 
     #绑定本地信息
     tcp_server_socket.bind(address)
@@ -37,21 +40,15 @@ def main():
     tcp_server_socket.listen(128)
 
     while True:
-        #等待客户端的连接:即为这个客户端发送文件:
-        client_socket,client_Addr = tcp_server_socket.accept()
+        #等待客户端的连接
+        new_client_socket,client_addr = tcp_server_socket.accept()
 
-        #接受对方发来的数据
-        recev_data = client_socket.recv(1024)
-        file_name = recev_data.decode("utf-8")
-        print("对方请求的文件名为: %s" % file_name)
-        file_content = get_file_content(file_name)
 
-        #发送文件的数据给客户端
-        if file_content:
-            client_socket.send(file_content)
+        #调用发送文件函数，完成为客户端服务
+        send_file_2_client(new_client_socket,client_addr)
 
-        #关闭这个套接字
-        client_socket.close()
+        #关闭套接字
+        new_client_socket.close()
 
     #关闭监听套接字
     tcp_server_socket.close()
