7a5f3ecc3d8a5668250124e9098a5e47bfb1acbf
GuangGuangLi <15607521232@163.com>
2019-12-06 18:16:49 +0800

    更新装饰器路由版加入数据库查询

--- 就业班/miniWeb框架_装饰器路由版/项目获取页面模板数据/dynamic/LG_mini_frame.py
+++ 就业班/miniWeb框架_装饰器路由版/项目获取页面模板数据/dynamic/LG_mini_frame.py
@@ -1,4 +1,6 @@
-
+#coding=utf-8
+import re
+from pymysql import connect
 
 """
 URL_FUNC_DICT = {
@@ -18,16 +20,70 @@ def route(url):#<<<<------------添加的装饰器
         return call_func
     return set_func
 
-@route("/login.html")#<<<<------------带参数的装饰器
-def login():
+@route("/index.html")#<<<<------------带参数的装饰器
+def index():
     with open("./templates/index.html",encoding="utf-8") as f:
         content = f.read()
+
+    conn = connect(host="localhost",user="root",password="123456",database="stock_db",charset="utf8")
+    cs = conn.cursor()
+    cs.execute("select * from info;")
+    stock_infos = cs.fetchall()
+    cs.close()
+    conn.close()
+    tr_template = """<tr>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>
+            <input type="button" value="添加" id="toAdd" name="toAdd" systemidvaule="000007">
+        </td>
+        </tr>
+        """
+    html = ""
+    for line_info in stock_infos:
+        html += tr_template % (line_info[0],line_info[1],line_info[2],line_info[3],line_info[4],line_info[5],line_info[6],line_info[7])
+    #content = re.sub(r"\{%content%\}",str(stock_infos),content)
+    content = re.sub(r"\{%content%\}", html, content)
     return content
 
 @route("/center.html")
 def center():
     with open("./templates/center.html",encoding="utf-8") as f:
         content = f.read()
+    conn = connect(host="localhost",user="root",password="123456",database="stock_db",charset="utf8")
+    cs = conn.cursor()
+    cs.execute("select i.code,i.short,i.chg,i.turnover,i.price,i.highs,f.note_info from info as i inner join focus as f on i.id=f.info_id;")
+    stock_infos = cs.fetchall()
+    cs.close()
+    conn.close()
+    tr_template = """<tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>
+                <a type="button" class="btn btn-default btn-xs" href="/update/300268.html"> <span class="glyphicon glyphicon-star" aria-hidden="true"></span> 修改 </a>
+            </td>
+            <td>
+                <input type="button" value="删除" id="toDel" name="toDel" systemidvaule="300268">
+            </td>
+        </tr>
+            """
+    html = ""
+    for line_info in stock_infos:
+        html += tr_template % (
+        line_info[0], line_info[1], line_info[2], line_info[3], line_info[4], line_info[5], line_info[6])
+    # content = re.sub(r"\{%content%\}",str(stock_infos),content)
+    content = re.sub(r"\{%content%\}", html, content)
     return content
 
 
