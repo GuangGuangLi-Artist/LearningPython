8ccf1c640dad0ce8b7aa315d6800166ee1db85a6
GuangGuangLi <15607521232@163.com>
2020-01-12 22:38:20 +0800

    Django的models

--- envDjango_test/blog/models.py
+++ envDjango_test/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+#设计和表对应的类，模型类
+
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    '''图书模型类'''
+    #CharField说明是一个字符串，max_length指定字符串的最大长度
+    btitle = models.CharField(max_length=20)
+
+    #说明是一个日期类型
+    bpub_date = models.DateField()
