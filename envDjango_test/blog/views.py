57cbf1d4f6ba63ab57db8a353abeacc93c24039f
GuangGuangLi <15607521232@163.com>
2020-01-17 20:56:18 +0800

    Django exercise

--- envDjango_test/blog/views.py
+++ envDjango_test/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,RequestContext
 
 # Create your views here.
 #定义视图函数，httprequest
@@ -8,7 +9,20 @@ from django.http import HttpResponse
 def index(request):
 
     #进行处理，和M和T进行交互
-    return HttpResponse('Django 学会了有什么用啊')
+    #return HttpResponse('Django 学会了有什么用啊')
+
+    #使用模板文件
+    #加载模板文件，模板对象
+    temp = loader.get_template('blog/index.html')
+    #定义模板上下文，给模板文件传递数据
+    context = RequestContext(request, {})
+
+    #模板渲染，产生标准的html内容
+    res_html = temp.render(context)
+
+    #返回给浏览器
+    return HttpResponse(res_html)
+
 
 
 def favoriteBook(requets):
