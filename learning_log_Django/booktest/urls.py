223810935742c94c76974fb55d372c885022bc5c
GuangGuangLi <15607521232@163.com>
2020-01-18 23:44:22 +0800

    learning_log_Django

---
+++ learning_log_Django/booktest/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import include, url
+from booktest import views
+
+
+urlpatterns = [
+   url(r'^index$',views.index),
+   url(r'^create$',views.create),
+   url(r'^delete(\d+)$',views.delete),
+
+]
