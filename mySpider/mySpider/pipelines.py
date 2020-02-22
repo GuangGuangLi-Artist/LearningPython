28eab180a0fbf05e32bfc666cbf2a908c3a6793d
GuangGuangLi <15607521232@163.com>
2020-02-22 17:03:03 +0800

    提交数据分析

--- mySpider/mySpider/pipelines.py
+++ mySpider/mySpider/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 class MyspiderPipeline(object):
     def process_item(self, item, spider):
--- 数据分析/matplotlib/matplotlib_exercise/matplotlib_draw.py
+++ 数据分析/matplotlib/matplotlib_exercise/matplotlib_draw.py
@@ -4,6 +4,16 @@ from matplotlib import pyplot as plt
 x = range(2,26,2)
 y = [15,13,14.5,17,20,25,26,26,27,22,18,15]
 
+fig = plt.figure(figsize=(32,20),dpi=100)
+
 plt.plot(x,y)
+
+#设置x轴的刻度
+_xtickets = [i/2 for i in range(4,49)]
+plt.xticks(_xtickets[::2])
+plt.yticks(range(min(y),max(y)+1))
+
+# plt.xticks(range(20,50))
+plt.savefig("./save_fig.png")
 
 plt.show()
